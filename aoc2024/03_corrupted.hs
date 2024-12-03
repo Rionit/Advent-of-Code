8655f38938207b21b649d338649cf5c0ff803caa
Filip Doležal <fidokadan@gmail.com>
2024-12-03 16:55:13 +0100

    Day 3 solution - Part 1

--- aoc2024/03_corrupted.hs
+++ aoc2024/03_corrupted.hs
@@ -1,7 +1,8 @@
 import AOCUtils (run)
 import Text.Parsec
 import Text.Parsec.String (Parser)
-import Data.Char 
+import Data.Char
+import Data.Maybe (catMaybes)
 
 inputFiles1 = ["test", "3_1"]
 inputFiles2 = inputFiles1
@@ -12,42 +13,64 @@ main = do
     -- run inputFiles2 parseInput solve2 2
 
 -- PARSING ----------------------------
-parseInput :: Parser String
-parseInput = many anyChar
+-- parseInput :: Parser String
+-- parseInput = many anyChar
+
+numberP :: Parser Int
+numberP = read <$> many1 digit
+
+parseMul = do
+    string "mul("
+    x <- numberP
+    char ','
+    y <- numberP
+    char ')'
+    return (x,y)
+
+mulOrCharP = try (Just <$> parseMul) <|> (anyChar >> return Nothing)
+
+parseInput = do
+    maybePairs <- many mulOrCharP
+    return $ catMaybes maybePairs
 
 -- SOLUTION 1 -------------------------
-scan :: String -> [String] -> String -> [String]
-scan [] res curr = res
-scan ('m':'u':'l':'(':xs) res _ = scan xs res "mul("
-scan (c:xs) res curr
-    | null curr = scan xs res ""
-    | '(' == last curr && isDigit c = scan xs res (curr ++ [c])
-    | ',' == last curr && isDigit c = scan xs res (curr ++ [c])
-    | isDigit (last curr) && isDigit c = scan xs res (curr ++ [c])
-    | c == ',' && isDigit (last curr) = scan xs res (curr ++ [c])
-    | c == ')' && isDigit (last curr) = scan xs (res ++ [curr ++ [c]]) ""
-    | otherwise = scan xs res ""
 
-split' :: (Char -> Bool) -> String -> [String]
-split' p s =  case dropWhile p s of
-                      "" -> []
-                      s' -> w : split' p s''
-                            where (w, s'') = break p s'
-
-extract :: String -> [Int]
-extract xs = map read $ concatMap (split' (==',')) $ rmBrackets $ rmMul xs
-    where 
-        rmBrackets xs = split' (=='(') $ concat $ split' (==')') xs
-        rmMul = filter (not . (`elem` "mul"))
-
-mul :: [Int] -> Int
-mul [] = 0                    
-mul [x] = x 
-mul (x:y:xs) = (x * y) + mul xs 
-
--- solve1 :: String -> Int
-solve1 input = mul $ extract $ concat $ scan input [""] ""
+solve1 :: [(Int, Int)] -> Int
+solve1 = foldr (\c acc -> acc + uncurry (*) c) 0
 -- SOLUTION 2 -------------------------
 -- solve2 :: [[Int]] -> Int
 
--- UTILS -------------------------
+-- NOT WORKING SOLUTION ---------------
+-- Doesn't work for some edge cases
+-- scan :: String -> [String] -> String -> [String]
+-- scan [] res curr = res
+-- scan ('m':'u':'l':'(':xs) res _ = scan xs res "mul("
+-- scan (c:xs) res curr
+--     | null curr = scan xs res ""
+--     | '(' == last curr && isDigit c = scan xs res (curr ++ [c])
+--     | ',' == last curr && isDigit c = scan xs res (curr ++ [c])
+--     | isDigit (last curr) && isDigit c = scan xs res (curr ++ [c])
+--     | c == ',' && isDigit (last curr) = scan xs res (curr ++ [c])
+--     | c == ')' && isDigit (last curr) = scan xs (res ++ [curr ++ [c]]) ""
+--     | otherwise = scan xs res ""
+
+-- split' :: (Char -> Bool) -> String -> [String]
+-- split' p s =  case dropWhile p s of
+--                       "" -> []
+--                       s' -> w : split' p s''
+--                             where (w, s'') = break p s'
+
+-- extract :: String -> [Int]
+-- extract xs = map read $ concatMap (split' (==',')) $ rmBrackets $ rmMul xs
+--     where
+--         rmBrackets xs = split' (=='(') $ concat $ split' (==')') xs
+--         rmMul = filter (not . (`elem` "mul"))
+
+-- unused since my first solution didn't work
+-- mul :: [Int] -> Int
+-- mul [] = 0                    
+-- mul [x] = x 
+-- mul (x:y:xs) = (x * y) + mul xs 
+
+-- solve1 :: String -> Int
+-- solve1 input = mul $ extract $ concat $ scan input [""] ""
