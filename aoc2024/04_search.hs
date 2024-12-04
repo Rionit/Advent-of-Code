059e705bbd43c3329ae18aa079f875293e511052
Filip Doležal <fidokadan@gmail.com>
2024-12-04 17:21:32 +0100

    Day 4 - solution Part 2 - Completely new solution...

--- aoc2024/04_search.hs
+++ aoc2024/04_search.hs
@@ -1,7 +1,9 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use head" #-}
 import AOCUtils (run)
 import Text.Parsec
 import Text.Parsec.String (Parser)
-import Data.List (transpose)
+import Data.List ( transpose, tails )
 import Data.Either
 import Data.Maybe (catMaybes)
 import Data.Foldable (traverse_)
@@ -12,7 +14,7 @@ inputFiles2 = inputFiles1
 main :: IO ()
 main = do
     run inputFiles1 parseInput solve1 1
-    -- run inputFiles2 parseInput2 solve2 2
+    run inputFiles2 parseInput solve2 2
 
 -- DATA TYPES / CLASSES --------------
 
@@ -28,7 +30,7 @@ parseLine = do
     optional endOfLine
     return line
 
-parseInput :: Parser [String] 
+parseInput :: Parser [String]
 parseInput = many1 parseLine
 
 -- SOLUTION 1 -------------------------
@@ -42,9 +44,12 @@ solve1 mat = sum $ catMaybes $ concat $ rights all'
         rows = concatMap (map (parse (many1 lineP) "")) [mat, map reverse mat]
         cols = concatMap (map (parse (many1 lineP) "")) [matT, map reverse matT]
         dias = concatMap (map (parse (many1 lineP) "")) [diagLR mat, diagLR matT, diagRL mat, map reverse $ diagRL mat]
-    
+
 -- SOLUTION 2 -------------------------
-solve2 = undefined
+solve2 :: [String] -> Int
+solve2 mat = sum $ concat $ mapi2d check
+    where
+        mapi2d f = zipWith (\y -> zipWith (\x e -> f e x y mat) [0..]) [0..] mat
 
 -- UTILS ------------------------------
 diagonals :: [[a]] -> [[a]]
@@ -53,3 +58,28 @@ diagonals [row] = map (: []) row
 diagonals (row : rows) = extendDiagonals row (diagonals rows)
   where
     extendDiagonals row diags = zipWith (:) row ([] : diags) ++ drop (length row - 1) diags
+
+check :: Char -> Int -> Int -> [String] -> Int
+check 'A' x y xss | all ((==True) . checkN) (neighbours x y xss) = 1
+                  | otherwise = 0
+check _ _ _ _ = 0
+
+slice :: Int -> Int -> [a] -> [a]
+slice from to xs = drop from $ take to xs
+
+extractDiagonals :: String -> [(Char, Char)]
+extractDiagonals str | length str == 9 = [firstPair, secondPair]
+                     | otherwise = [('x','x')]
+    where
+        firstPair = (str !! 0, str !! 8)
+        secondPair = (str !! 2, str !! 6)
+
+neighbours :: Int -> Int -> [String] -> [(Char, Char)]
+neighbours x y board = extractDiagonals sliced
+    where
+        sliced = slice (y-1) (y+2) board >>= slice (x-1) (x+2)
+
+checkN :: (Char, Char) -> Bool
+checkN ('M', 'S') = True
+checkN ('S', 'M') = True
+checkN (_, _) = False
