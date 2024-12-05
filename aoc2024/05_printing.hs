82b1eaed7b9807d4a3da950f42cb64a69bd0dc05
Doležal, Filip <dolezfil@fel.cvut.cz>
2024-12-05 12:27:40 +0100

    Day 5 - part 2 solution

--- aoc2024/05_printing.hs
+++ aoc2024/05_printing.hs
@@ -9,7 +9,7 @@ inputFiles2 = inputFiles1
 main :: IO ()
 main = do
     run inputFiles1 parseInput solve1 1
-    -- run inputFiles2 parseInput solve2 2
+    run inputFiles2 parseInput solve2 2
 
 -- DATA TYPES / CLASSES --------------
 
@@ -38,7 +38,6 @@ parseInput = do
     updates <- many1 parseUpdate
     return (rules, updates)
 
--- check (IntMap.lookup 0 rulebook)
 -- SOLUTION 1 -------------------------
 solve1 (rules, updates) = sum
                         $ map (\xs -> xs !! (length xs `div` 2))
@@ -49,17 +48,27 @@ solve1 (rules, updates) = sum
         step prev page = prev++[check (IntMap.lookup page rulebook) page (map fst prev)]
         rulebook = IntMap.fromListWith (++) [(k, [v]) | (k, v) <- rules]
 -- SOLUTION 2 -------------------------
-solve2 = undefined
+solve2 (rules, updates) = sum
+                        $ map (\xs -> xs !! (length xs `div` 2))
+                        $ map (map fst)
+                        $ map (foldl step2 [])
+                        $ filter (not . all snd)
+                        $ map (foldl step1 []) updates
+    where
+        step2 prev page@(k, state) | state == True = prev++[page]
+                                   | otherwise = fix prev page rulebook
+        step1 prev page = prev++[check (IntMap.lookup page rulebook) page (map fst prev)]
+        rulebook = IntMap.fromListWith (++) [(k, [v]) | (k, v) <- rules]
 -- UTILS ------------------------------
+
+fix :: [(Int, Bool)] -> (Int, Bool) -> IntMap.IntMap [Int] -> [(Int, Bool)]
+fix prev page@(k, state) r | state' == True = prev'++[page']++[last prev]
+                           | otherwise = (fix prev' page r)++[last prev]
+    where
+        page'@(k', state') = check (IntMap.lookup k r) k (map fst prev')
+        prev' = init prev
+
 check :: Maybe [Int] -> Int -> [Int] -> (Int, Bool)
 check (Just v) k prev | all (not) $ map (`elem` prev) v = (k, True)
                       | otherwise = (k, False)
 check Nothing k _ = (k, True)
-
-slice :: Int -> Int -> [a] -> [a]
-slice from to xs = drop from $ take to xs
-
--- neighbours :: Int -> Int -> [String] -> [(Char, Char)]
--- neighbours x y board = extractDiagonals sliced
---     where
---         sliced = slice (y-1) (y+2) board >>= slice (x-1) (x+2)
