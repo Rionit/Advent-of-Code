825a0fe75d28f478e45936dda946d6361fd819e3
Doležal, Filip <dolezfil@fel.cvut.cz>
2024-12-07 15:17:11 +0100

    Day 7 - part 2 - No time for omptimizing

--- aoc2024/07_formulas.hs
+++ aoc2024/07_formulas.hs
@@ -10,7 +10,7 @@ inputFiles2 = inputFiles1
 main :: IO ()
 main = do
     run inputFiles1 parseInput solve1 1
-    -- run inputFiles2 parseInput solve2 2
+    run inputFiles2 parseInput solve2 2
 
 -- DATA TYPES / CLASSES ---------------
 
@@ -30,16 +30,21 @@ parseInput :: Parser [(Int, [Int])]
 parseInput = many1 parseLine
 
 -- SOLUTION 1 -------------------------
-solve1 input = sum $ map (\(test, xs) -> eval [head xs] (tail xs) test) input
-        
+solve1 input = sum $ map (\(test, xs) -> eval [head xs] (tail xs) test "*+") input
 -- SOLUTION 2 -------------------------
-solve2 input = input
+-- Could optimize by using '||' only on failed results from part 1 (should be half)
+-- Don't have time to implement :(
+solve2 input = sum $ map (\(test, xs) -> eval [head xs] (tail xs) test "*+|") input
 -- UTILS ------------------------------
 op :: Int -> Char -> Int -> Int
 op x '*' y = x * y
 op x '+' y = x + y
+op x '|' y = x `join` y
 
-eval :: [Int] -> [Int] -> Int -> Int
-eval [] _ test = 0
-eval xs [] test = if any (== test) xs then test else 0
-eval xs (y:ys) test = eval [result | x <- xs, o <- "*+", let result = op x o y, result <= test] ys test
+join :: Int -> Int -> Int
+join x y = read ((show x) ++ (show y))
+
+eval :: [Int] -> [Int] -> Int -> String -> Int
+eval [] _ test _ = 0
+eval xs [] test _ = if any (== test) xs then test else 0
+eval xs (y:ys) test ops = eval [result | x <- xs, o <- ops, let result = op x o y, result <= test] ys test ops
