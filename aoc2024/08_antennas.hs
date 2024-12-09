f42bc4e5fdfc11bfb28e7da6f39a0e184dea78b3
Filip Doležal <fidokadan@gmail.com>
2024-12-09 20:44:50 +0100

    Day 8 - part 1 solution

--- aoc2024/08_antennas.hs
+++ aoc2024/08_antennas.hs
@@ -1,11 +1,10 @@
 import AOCUtils (run)
 import Data.List
 import Text.Parsec
+import Data.Function
 import Text.Parsec.String (Parser)
-import Data.Maybe (isJust, catMaybes, fromJust)
 
--- inputFiles1 = ["test", "8_1"]
-inputFiles1 = ["test"]
+inputFiles1 = ["test", "8_1"]
 inputFiles2 = inputFiles1
 
 main :: IO ()
@@ -16,14 +15,34 @@ main = do
 -- DATA TYPES / CLASSES ---------------
 
 -- PARSING ----------------------------
+getPos :: Parser (Line, Column)
+getPos = do
+    posI <- getPosition
+    return (sourceLine posI - 1, sourceColumn posI - 1)
 
-parseInput :: Parser Char
-parseInput = anyChar
+antennaP :: Parser (Char, (Int, Int))
+antennaP = do
+    many $ oneOf ".\n"
+    position <- getPos
+    antenna <- noneOf ".\n"
+    many $ oneOf ".\n"
+    return (antenna, position)
+
+parseInput :: Parser (Int, Int, [(Char, (Int, Int))])
+parseInput = do
+    antennas <- many1 antennaP
+    dimensions@(rows, cols) <- getPos
+    return (rows, cols - 1, antennas)
 
 -- SOLUTION 1 -------------------------
-solve1 input = input
+solve1 :: (Int, Int, [(Char, (Int, Int))]) -> Int
+solve1 (rows, cols, antennas) = length . group . sort $ concatMap (filter bounds . antinodes) $ grouped antennas
+    where
+        grouped = map (map snd) . groupBy ((==) `on` fst) . sort
+        bounds (x, y) = x `elem` [0 .. cols] && y `elem` [0 .. rows]
 -- SOLUTION 2 -------------------------
--- Could optimize by using '||' only on failed results from part 1 (should be half)
--- Don't have time to implement :(
 solve2 input = input
 -- UTILS ------------------------------
+antinodes xs = concatMap (\((x1, y1), (x2, y2)) -> [(2*x1 - x2, 2*y1 - y2), (2*x2 - x1, 2*y2 - y1)]) pairs
+  where
+    pairs = [(x, y) | (x:ys) <- tails xs, y <- ys]
