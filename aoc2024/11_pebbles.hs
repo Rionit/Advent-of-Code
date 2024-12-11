0153eab8021e595f6c27fb5da84568262ee58220
Filip Doležal <fidokadan@gmail.com>
2024-12-11 19:41:29 +0100

    Day 11 - part 2 solution

--- aoc2024/11_pebbles.hs
+++ aoc2024/11_pebbles.hs
@@ -1,20 +1,18 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use <&>" #-}
+{-# LANGUAGE TupleSections #-}
 import AOCUtils (run)
-import Data.List
-import Data.Char
-import Text.Parsec
-import Data.Function
+import Data.List ( group, groupBy, sort )
+import Text.Parsec ( char, digit, many1, sepEndBy1 )
+import Data.Function ( on )
 import Text.Parsec.String (Parser)
-import Data.Array
+import qualified Data.Map as Map
 
 inputFiles1 = ["test", "11_1"]
-inputFiles2 = inputFiles1
+inputFiles2 = ["11_1"]
 
 main :: IO ()
 main = do
     run inputFiles1 parseInput solve1 1
-    -- run inputFiles2 parseInput solve2 2
+    run inputFiles2 parseInput solve2 2
 
 -- DATA TYPES / CLASSES ---------------
 -- PARSING ----------------------------
@@ -23,11 +21,23 @@ numberP = read <$> many1 digit
 
 parseInput :: Parser [Int]
 parseInput = sepEndBy1 numberP (char ' ')
+
 -- SOLUTION 1 -------------------------
 solve1 :: [Int] -> Int
-solve1 input = length $ foldr (\c acc -> concatMap blink acc) input [1..25]
+solve1 = solve 25
+
 -- SOLUTION 2 -------------------------
-solve2 input = input
+solve2 :: [Int] -> Int
+solve2 = solve 75
+
+solve :: Int -> [Int] -> Int
+solve n input = sum $ Map.elems $ foldr step (countsMap $ group $ sort input) [1..n]
+  where
+    grouped = groupBy ((==) `on` fst)
+    countsMap xs = Map.fromList [(head x, length x) | x <- xs]
+    step c acc = Map.fromList $ map (\xs -> (fst (head xs), sum (map snd xs))) $ grouped $ sort $ concat $ zipWith blinked (Map.keys acc) (Map.elems acc)
+    blinked k e = map (, e) (blink k)
+
 -- UTILS ------------------------------
 split' :: Int -> [Int]
 split' n = let
@@ -46,4 +56,4 @@ fromDigits = foldl (\acc d -> acc * 10 + d) 0
 blink :: Int -> [Int]
 blink 0 = [1]
 blink pebble | even $ length $ toDigits pebble = split' pebble
-             | otherwise = [pebble*2024]
+             | otherwise = [pebble * 2024]
